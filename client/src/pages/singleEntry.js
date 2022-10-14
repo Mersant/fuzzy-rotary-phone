0ad0fa23c0a142fad14134a50112b51442189994
omarfarah123 <76454677+omarfarah123@users.noreply.github.com>
2022-10-14 17:54:15 -0400

    Created singleEntry.js

---
+++ client/src/pages/singleEntry.js
@@ -0,0 +1,43 @@
+import React from 'react';
+
+
+import { useParams } from 'react-router-dom';
+import { useQuery } from '@apollo/client';
+
+import { QUERY_SINGLE_JOURNAL } from '../utils/queries';
+
+const SingleJournal = () => {
+
+  const { journalId } = useParams();
+
+  const { loading, data } = useQuery(QUERY_SINGLE_JOURNAL, {
+
+    variables: { journalId: journalId },
+  });
+
+  const journal = data?.journal || {};
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+  return (
+    <div className="my-3">
+      <div className="bg-light py-4">
+        <blockquote
+          className="p-4"
+          style={{
+            fontSize: '1.5rem',
+            fontStyle: 'italic',
+            border: '2px dotted #1a1a1a',
+            lineHeight: '1.5',
+          }}
+        >
+          {journal.journalText}
+        </blockquote>
+      </div>
+
+    </div>
+  );
+};
+
+export default SingleJournal;
