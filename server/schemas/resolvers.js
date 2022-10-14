d7b90c3c2735b40d6b172c92b25a70acd699d5fb
omarfarah123 <76454677+omarfarah123@users.noreply.github.com>
2022-10-14 11:42:07 -0400

    Created Resolvers.js

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,69 @@
+const { AuthenticationError } = require('apollo-server-express');
+const { User, Journal } = require('../models');
+const { signToken } = require('../utils/auth');
+
+const resolvers = {
+  Query: {
+    users: async () => {
+      return User.find().populate('journal');
+    },
+    user: async (parent, { username }) => {
+      return User.findOne({ username }).populate('journal');
+    },
+    journals: async (parent, { username }) => {
+      const params = username ? { username } : {};
+      return Journal.find(params).sort({ createdAt: -1 });
+    },
+    journal: async (parent, { journalId }) => {
+      return Journal.findOne({ _id: journalId });
+    },
+    dme: async (parent, args, context) => {
+      if (context.user) {
+        return User.findOne({ _id: context.user._id }).populate('journal');
+      }
+      throw new AuthenticationError('You need to be logged in!');
+    },
+  },
+
+  Mutation: {
+    addUser: async (parent, { username, email, password }) => {
+      const user = await User.create({ username, email, password });
+      const token = signToken(user);
+      return { token, user };
+    },
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw new AuthenticationError('No user found with this email address');
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw new AuthenticationError('Incorrect credentials');
+      }
+
+      const token = signToken(user);
+
+      return { token, user };
+    },
+    addJournal: async (parent, { journalText }, context) => {
+      if (context.user) {
+        const journal = await Journal.create({
+          journalText
+        });
+
+        await User.findOneAndUpdate(
+          { _id: context.user._id },
+          { $addToSet: { journal: journal._id } }
+        );
+
+        return journal;
+      }
+      throw new AuthenticationError('You need to be logged in!');
+    },
+  },
+};
+
+module.exports = resolvers;
