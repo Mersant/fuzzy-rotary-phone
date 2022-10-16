2807eadeb6e2fb0ddb73b09f934b27256368676c
Mersant <dlowry99@outlook.com>
2022-10-16 17:02:22 -0400

    Added cool header styling

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
+import '../../styles/Header.css'
 import Auth from '../../utils/auth';
 
 
@@ -16,26 +16,26 @@ return (
     <div className="container flex-row justify-space-between-lg justify-center align-center">
       <div className= "container">
         <Link className="text-light" to="/">
-          <h1 className="m-0">Journal</h1>
+          <h1 className="m-0">Letters to Quandale</h1>
         </Link>
       </div>
       <div class= "">
       {Auth.loggedIn() ? (
             <>
               {/* <span>Hello {Auth.getProfile().data.username}!</span> */}
-              <button class="btn btn-lg btn-light m-2" onClick={logout}>
+              <button class="btn btn-lg btn-light m-2 headerButton" onClick={logout}>
                 Logout
               </button>
             </>
           ) : (
               <>
-              <Link className="btn btn-lg btn-info m-2" to="/login">
+              <Link className="btn btn-lg btn-info m-2 headerButton" to="/login">
                 Login
               </Link>
-              <Link className="btn btn-lg btn m-2" to="/signup">
+              <Link className="btn btn-lg btn m-2 headerButton" to="/signup">
                 Sign up
               </Link>
-              <Link className="btn btn-lg btn m-2" to="/journal">
+              <Link className="btn btn-lg btn m-2 headerButton" to="/journal">
                 Journal
               </Link>
             </>
