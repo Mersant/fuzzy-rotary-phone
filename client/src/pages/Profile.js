aa4c9839c4622ca5cb8e8ed3c02ee1903d5d6250
Mersant <dlowry99@outlook.com>
2022-10-16 21:04:13 -0400

    Styled profile page

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -4,42 +4,46 @@ import { useQuery } from '@apollo/client';
 import AllJournals from '../components/AllJournals';
 import { QUERY_USER, QUERY_ME } from '../utils/queries';
 import Auth from '../utils/auth';
+import '../styles/Profile.css'
 
 const Profile = () => {
-    const userData  = Auth.getProfile;
+  const userData = Auth.getProfile;
 
- const {loading, data} = useQuery(QUERY_ME)
- const user = data?.me  || {};
-    // if(Auth.loggedIn())
-    // {return <Navigate to ="/myJournals"/>}  //infinitely loops
-    
+  const { loading, data } = useQuery(QUERY_ME)
+  const user = data?.me || {};
+  // if(Auth.loggedIn())
+  // {return <Navigate to ="/myJournals"/>}  //infinitely loops
 
-    if(loading){
-        return<div>Fetching those journals....</div>;
-    }
-    if(!Auth.loggedIn){
-        return(
-            <h1>
-                You need to be logged in to view your journals! Please log in!
-            </h1>
-        );
-    }
-  
+
+  if (loading) {
+    return <div>Fetching those journals....</div>;
+  }
+  if (!Auth.loggedIn) {
     return (
-        <div>
-        <div className="flex-row justify-center mb-3">
-          <h2 className="col-12 col-md-10 bg-dark text-light p-3 mb-5">
+      <h1>
+        You need to be logged in to view your journals! Please log in!
+      </h1>
+    );
+  }
+
+  return (
+    <div>
+      <div className="flex-row justify-center mb-3">
+        <div className='justify-center'>
+          <h2 className="col-12 col-md-10 bg-dark text-light p-3 mb-5 myJournalsHeader">
             Viewing Your Profile.
           </h2>
-  
-          <div className="col-12 col-md-10 mb-5">
-            <AllJournals
-              journals={user.journal}
-            />
-          </div>
         </div>
       </div>
-    )
+
+      <div className="flex-row col-12 col-md-10 mb-5 justify-center">
+        <AllJournals
+          journals={user.journal}
+        />
+      </div>
+    </div>
+
+  )
 }
 
 export default Profile
