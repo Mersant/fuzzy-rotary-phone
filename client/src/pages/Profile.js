df675047b8775ae95b3e2c10897509e9b78341ac
Jacob Karla <jacob.l.karla@gmail.com>
2022-10-16 20:24:04 -0400

    modified resolvers to properly attach journals to users

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,19 +1,15 @@
 import React from 'react';
 import { Navigate, useParams } from 'react-router-dom';
 import { useQuery } from '@apollo/client';
-
 import AllJournals from '../components/AllJournals';
-
 import { QUERY_USER, QUERY_ME } from '../utils/queries';
-
 import Auth from '../utils/auth';
 
 const Profile = () => {
     const userData  = Auth.getProfile;
 
  const {loading, data} = useQuery(QUERY_ME)
- const user = data?.me || data?.user || {};
-    
+ const user = data?.me  || {};
     // if(Auth.loggedIn())
     // {return <Navigate to ="/myJournals"/>}  //infinitely loops
     
@@ -21,24 +17,19 @@ const Profile = () => {
     if(loading){
         return<div>Fetching those journals....</div>;
     }
-    if(!user?.username){
+    if(!Auth.loggedIn){
         return(
             <h1>
                 You need to be logged in to view your journals! Please log in!
             </h1>
         );
     }
+  
     return (
         <div>
-<<<<<<< HEAD
-        <div class="flex-row justify-center mb-3">
-          <h2 class="col-12 col-md-10 bg-dark text-light p-3 mb-5">
-            Viewing {`${userData.data.username}'s`} profile.
-=======
         <div className="flex-row justify-center mb-3">
           <h2 className="col-12 col-md-10 bg-dark text-light p-3 mb-5">
-            Viewing {`${user.username}'s`} profile.
->>>>>>> d595147bcd260958d115090696db48baa3d0b710
+            Viewing Your Journals!.
           </h2>
   
           <div className="col-12 col-md-10 mb-5">
